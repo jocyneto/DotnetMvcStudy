a018e4296fbcca2d1eb2ed431e4b925fa409f4da
Jocy Gonçalves Neto - PC <jocygneto@gmail.com>
2022-12-23 15:19:19 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -10,5 +10,28 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public ICollection<Seller> Sellers { get; set; }
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void addSeller(Seller s)
+        {
+            Sellers.Add(s);
+        }
+
+        public double TotalSalesSeller(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.totalSales(initial, final));
+        }
+
     }
 }
